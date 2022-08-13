50170f20eaa80a2f3e7d8b7185e6dc060b98ae2a
soyacen <soyacen@outlook.com>
2022-08-13 10:52:22 +0800

    rm Serialize

--- mutex.go
+++ mutex.go
@@ -1,10 +1,7 @@
 package cache
 
 import (
-	"encoding/gob"
 	"fmt"
-	"io"
-	"os"
 	"runtime"
 	"sync"
 	"time"
@@ -383,84 +380,6 @@ func (c *mutexCache) Flush() {
 	c.mu.Lock()
 	c.items = map[string]Item{}
 	c.mu.Unlock()
-}
-
-// Write the cache's items (using Gob) to an io.Writer.
-//
-// NOTE: This method is deprecated in favor of c.Items() and newMutexCacheFrom() (see the
-// documentation for newMutexCacheFrom().)
-func (c *mutexCache) Save(w io.Writer) (err error) {
-	enc := gob.NewEncoder(w)
-	defer func() {
-		if x := recover(); x != nil {
-			err = fmt.Errorf("Error registering item types with Gob library")
-		}
-	}()
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	for _, v := range c.items {
-		gob.Register(v.Object)
-	}
-	err = enc.Encode(&c.items)
-	return
-}
-
-// Save the cache's items to the given filename, creating the file if it
-// doesn't exist, and overwriting it if it does.
-//
-// NOTE: This method is deprecated in favor of c.Items() and newMutexCacheFrom() (see the
-// documentation for newMutexCacheFrom().)
-func (c *mutexCache) SaveFile(fname string) error {
-	fp, err := os.Create(fname)
-	if err != nil {
-		return err
-	}
-	err = c.Save(fp)
-	if err != nil {
-		fp.Close()
-		return err
-	}
-	return fp.Close()
-}
-
-// Add (Gob-serialized) cache items from an io.Reader, excluding any items with
-// keys that already exist (and haven't expired) in the current cache.
-//
-// NOTE: This method is deprecated in favor of c.Items() and newMutexCacheFrom() (see the
-// documentation for newMutexCacheFrom().)
-func (c *mutexCache) Load(r io.Reader) error {
-	dec := gob.NewDecoder(r)
-	items := map[string]Item{}
-	err := dec.Decode(&items)
-	if err == nil {
-		c.mu.Lock()
-		defer c.mu.Unlock()
-		for k, v := range items {
-			ov, found := c.items[k]
-			if !found || ov.Expired() {
-				c.items[k] = v
-			}
-		}
-	}
-	return err
-}
-
-// Load and add cache items from the given filename, excluding any items with
-// keys that already exist in the current cache.
-//
-// NOTE: This method is deprecated in favor of c.Items() and newMutexCacheFrom() (see the
-// documentation for newMutexCacheFrom().)
-func (c *mutexCache) LoadFile(fname string) error {
-	fp, err := os.Open(fname)
-	if err != nil {
-		return err
-	}
-	err = c.Load(fp)
-	if err != nil {
-		fp.Close()
-		return err
-	}
-	return fp.Close()
 }
 
 type janitor struct {
